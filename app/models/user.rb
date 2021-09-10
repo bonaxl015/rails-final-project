c1b5a2bfec2579adc0d4a53f8774f2886ece7f60
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-10 18:40:39 +0800

    Created User model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3 },
+                       format: { with: /\A[A-Za-z0-9]+\z/ }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+end
