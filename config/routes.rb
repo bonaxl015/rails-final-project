4fe3186248ce7efdfb5f2ed1285273c7dce63d36
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-11 17:00:09 +0800

    Added routes for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root to: 'static#home'
   get 'static/home'
   devise_for :users
+  resources :dashboard do
+    collection do
+      get :news_feed
+      get :profile
+    end
+  end
 end
