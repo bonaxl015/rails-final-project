96e73493e49b3f77acb0de0c4438c2d45711ac72
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-11 10:31:37 +0800

    Added a condition to display headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def display_headers?(current_controller)
+    %w[sessions registrations passwords static].any? { |controller| controller.include? current_controller }
+  end
 end
