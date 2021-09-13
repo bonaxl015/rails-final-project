02700ca60b25e37923ec1ebdb2c8f2dcb774c489
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-13 19:40:28 +0800

    Add events controller and helper

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,69 @@
+class EventsController < ApplicationController
+  load_and_authorize_resource except: %i[index show all]
+  before_action :authenticate_user!
+  before_action :set_event, only: %i[show edit update destroy]
+
+  def index
+    @user_events = Event.where(user_id: current_user)
+    @events = Event.all
+  end
+
+  def all
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def edit
+  end
+
+  def create
+    @event = Event.new(event_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: "Event was successfully updated." }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date)
+  end
+end
