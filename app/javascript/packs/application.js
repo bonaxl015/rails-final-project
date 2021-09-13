2146c849a7b557ba1c7cc18840862303025011d2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-13 19:13:01 +0800

    Add jqueryui and fix jquery config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,14 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "jquery"
+import "jquery-ui"
+import "packs/head"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
