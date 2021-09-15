ec04f48b48742ee332d63c4d366968419dfbaaf2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-15 21:22:34 +0800

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@ admin = User.create(  email: 'admin@example.com',
                       password_confirmation: 'testtest',
                       username: 'adminpogi',
                       first_name: 'admin',
-                      last_name: 'admin'
+                      last_name: 'admin',
+                      slug: 'adminpogi'
                     )
 
 moderator = User.create(  email: 'moderator@example.com',
@@ -15,7 +16,8 @@ moderator = User.create(  email: 'moderator@example.com',
                           password_confirmation: 'testtest',
                           username: 'moderator',
                           first_name: 'moderator',
-                          last_name: 'moderator'
+                          last_name: 'moderator',
+                          slug: 'moderator'
                         )
 
 ordinary = User.create( email: 'bon@example.com',
@@ -23,20 +25,23 @@ ordinary = User.create( email: 'bon@example.com',
                         password_confirmation: 'testtest',
                         username: 'bonaxlpogi',
                         first_name: 'Bon Axl',
-                        last_name: 'Feeser'
+                        last_name: 'Feeser',
+                        slug: 'bonaxlpogi'
 )
 
 ordinary2 = User.create( email: 'jane@example.com',
                          password: 'testtest',
                          username: 'janedoe',
                          first_name: 'Jane',
-                         last_name: 'Doe')
+                         last_name: 'Doe',
+                         slug: 'janedoe')
 
 ordinary3 = User.create( email: 'juan@example.com',
                          password: 'testtest',
                          username: 'juandcruz',
                          first_name: 'Juan',
-                         last_name: 'Dela Cruz')
+                         last_name: 'Dela Cruz',
+                         slug: 'juandcruz')
 
 admin_role = Role.create(role: 'Admin')
 moderator_role = Role.create(role: 'Moderator')
@@ -74,3 +79,5 @@ Attend.create(
 
 Relationship.create(follower_id: ordinary2.id, followed_id: ordinary.id)
 Relationship.create(follower_id: ordinary3.id, followed_id: ordinary.id)
+
+User.find_each(&:save)
