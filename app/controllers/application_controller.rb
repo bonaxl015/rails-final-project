015d9658e701d63e435f5ac51c60f78e4f139b1d
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-15 16:36:43 +0800

    Add relationship method and modify profile method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def profile
+    @user = User.find(params[:id])
+    @user_posts = Post.where(user_id: params[:id]).order(created_at: :desc).includes(:user)
+  end
+
+  def relationships
+    @user = User.find(params[:id])
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username first_name last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[username first_name last_name])
