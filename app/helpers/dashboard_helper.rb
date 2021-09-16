4de63303fc2f975f6fef22193063824ddd882a19
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-16 16:39:10 +0800

    Removed unnecessary files

--- app/helpers/dashboard_helper.rb
+++
@@ -1,5 +0,0 @@
-module DashboardHelper
-  def distance_of_time_posted_in_words(from_time, to_time)
-    distance_of_time_in_words(from_time, to_time).concat(' ago')
-  end
-end
--- app/javascript/packs/news-feed.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-  $("#caption").on("keyup", function () {
-    if ($("#caption").val() != "")
-      $("#create-post-button").prop("disabled", false);
-    else $("#create-post-button").prop("disabled", true);
-  });
-});
