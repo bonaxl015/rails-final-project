318238715b10b2ce3c45457a76b1da0cbe977ef9
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-17 15:07:32 +0800

    Added controller methods for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.new(comment_params.merge(user_id: current_user.id))
+    if @comment.save
+      respond_to do |format|
+        format.html { redirect_to posts_path }
+      end
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_path, notice: 'Your comment was successfully removed.' }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
