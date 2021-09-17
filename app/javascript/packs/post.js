ad99820a137aece9d5ac226091ff2390c4684a0e
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-17 10:07:37 +0800

    Add jquery for like button and modal

--- app/javascript/packs/post.js
+++ app/javascript/packs/post.js
@@ -8,4 +8,12 @@ $(function () {
       $("#create-post-button").prop("disabled", false);
     else $("#create-post-button").prop("disabled", true);
   });
+
+  $(".likers-btn").on("click", function () {
+    $(this).addClass("active");
+  });
+
+  $(".close-likers-btn").on("click", function () {
+    $(".likers-btn").removeClass("active");
+  })
 });
