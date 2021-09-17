dc7984e83749d86d905e429d1b5b30ec8b37a090
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-17 15:18:12 +0800

    Added condition for disabling comment button when comment box is empty. Created a text toggle to show and hide comments

--- app/javascript/packs/post.js
+++ app/javascript/packs/post.js
@@ -8,4 +8,22 @@ $(function () {
       $("#create-post-button").prop("disabled", false);
     else $("#create-post-button").prop("disabled", true);
   });
+
+  if ($(".comment").val() == "") {
+    $(".create-comment-button").prop("disabled", true);
+  }
+
+  $(".comment").on("keyup", function () {
+    if ($(this).val() != "")
+      $(this).closest(".comment-text-button").find(".create-comment-button").prop("disabled", false);
+    else
+      $(this).closest(".comment-text-button").find(".create-comment-button").prop("disabled", true);
+  });
+
+  $(".comment-toggle").on("click", function () {
+    $(this).closest(".comment-box").children(".comment-display").toggle("slow");
+    if ($(".comment-toggle").text() == "View Comments")
+      $(".comment-toggle").text("Hide Comments");
+    else $(".comment-toggle").text("View Comments");
+  });
 });
