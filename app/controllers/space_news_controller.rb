29327b0087358fffbbe7944af2e8e37e03eef634
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-20 22:44:08 +0800

    Fix rubocop errors

--- app/controllers/space_news_controller.rb
+++ app/controllers/space_news_controller.rb
@@ -12,6 +12,7 @@ class SpaceNewsController < ApplicationController
 
   def search
     return unless params[:title]
+
     data = SpaceFlight.new.article_search(params[:title]).body
     @news = parse(data).paginate(page: params[:page], per_page: 12)
     render(action: :index)
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   def home
     return unless user_signed_in?
+
     redirect_to posts_path
   end
 end
