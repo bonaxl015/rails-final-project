f42e94c530970f76a23404b86f7232d060ed8edf
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-20 22:21:49 +0800

    Created controller for space news

---
+++ app/controllers/space_news_controller.rb
@@ -0,0 +1,20 @@
+require 'space_flight'
+require 'will_paginate/array'
+
+class SpaceNewsController < ApplicationController
+  before_action :authenticate_user!
+  include SpaceNewsHelper
+
+  def index
+    data = SpaceFlight.new.article_limit(240).body
+    @news = parse(data).paginate(page: params[:page], per_page: 12)
+  end
+
+  def search
+    if params[:title]
+      data = SpaceFlight.new.article_search(params[:title]).body
+      @news = parse(data).paginate(page: params[:page], per_page: 12)
+      render(action: :index)
+    end
+  end
+end
