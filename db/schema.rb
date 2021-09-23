28adc3cc704546cbaa0a5f1a474773d66ba4341e
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-23 21:05:11 +0800

    Updated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_092915) do
+ActiveRecord::Schema.define(version: 2021_09_23_025752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 2021_09_18_092915) do
     t.string "first_name"
     t.string "last_name"
     t.string "slug"
+    t.string "profile_pic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
