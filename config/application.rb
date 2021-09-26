84e0e0fb502708c3bbd6e9166e456959c0ecb18a
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-26 23:20:12 +0800

    Include lib folder on initilization

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module FinalProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.exceptions_app = self.routes
+    config.autoload_paths += %W(#{config.root}/lib)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
