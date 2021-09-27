e64f9d40c798cc18d5cdd165e801193e116fad4f
Bon Axl Feeser <bonaxl015@gmail.com>
2021-09-27 21:45:13 +0800

    Added conditional redirection based on previous controller and action visited

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,14 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(@user)
-    redirect_to profile_path(@user)
+    redirect_to profile_path(@user) if session[:relationship_previous_controller] == 'devise' && session[:relationship_previous_action] == 'profile'
+    redirect_to user_search_posts_path(username: session[:username], commit: session[:commit]) if session[:relationship_previous_controller] == 'posts' && session[:relationship_previous_action] == 'user_search'
   end
 
   def destroy
     current_user.unfollow(@user)
-    redirect_to profile_path(@user)
+    redirect_to profile_path(@user) if session[:relationship_previous_controller] == 'devise' && session[:relationship_previous_action] == 'profile'
+    redirect_to user_search_posts_path(username: session[:username], commit: session[:commit]) if session[:relationship_previous_controller] == 'posts' && session[:relationship_previous_action] == 'user_search'
   end
 
   private
