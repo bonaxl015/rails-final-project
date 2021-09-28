29a7babe2205ee025aab5e88ca3acccef76dd4a6
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-28 12:24:43 +0800

    Fix rubocop error

--- spec/requests/space_news_spec.rb
+++ spec/requests/space_news_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'SpaceNews', type: :request do
-
   context 'when user signed in' do
     shared_examples 'renders a successful response' do
       it 'renders a successful response' do
