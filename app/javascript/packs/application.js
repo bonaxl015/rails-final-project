d69e9dbe97b7bd660c285b10aa04ba8b1dcc0632
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-28 14:41:13 +0800

    Fix workflow errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
@@ -20,5 +20,5 @@ import "packs/event"
 global.$ = jQuery
 
 Rails.start()
-// Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
