254b8ff043680478d6f2716bdf90d5ff616f13a2
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-09-28 12:22:39 +0800

    Add requests test

--- spec/requests/space_news_spec.rb
+++ spec/requests/space_news_spec.rb
@@ -1,7 +1,44 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "SpaceNews", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe 'SpaceNews', type: :request do
+
+  context 'when user signed in' do
+    shared_examples 'renders a successful response' do
+      it 'renders a successful response' do
+        expect(response).to be_successful
+      end
+    end
+
+    before do
+      sign_in create(:user)
+    end
+
+    describe 'GET /index' do
+      before do
+        get space_news_index_path
+      end
+
+      include_examples 'renders a successful response'
+    end
+
+    describe 'GET /search' do
+      before do
+        get search_space_news_index_path, params: { title: 'status', commit: 'Search' }
+      end
+
+      include_examples 'renders a successful response'
+    end
+  end
+
+  context 'when user not signed in' do
+    describe 'GET /index' do
+      before do
+        get space_news_index_path
+      end
+
+      it 'redirects to home' do
+        expect(response).to redirect_to(static_home_path)
+      end
+    end
+  end
+end
