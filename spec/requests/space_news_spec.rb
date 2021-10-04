f8ee8bb8a00d9c3ad95c4283d94d26fdeedf3b4d
Patrick Samson <79001601+patricklsamson@users.noreply.github.com>
2021-10-04 23:35:48 +0800

    Update search space news requests test

--- spec/requests/space_news_spec.rb
+++ spec/requests/space_news_spec.rb
@@ -1,28 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'SpaceNews', type: :request do
-  context 'when user signed in' do
-    shared_examples 'renders a successful response' do
-      it 'renders a successful response' do
-        expect(response).to be_successful
-      end
-    end
-
+  shared_context 'when user signed in' do
     before do
       sign_in create(:user)
     end
+  end
 
-    describe 'GET /index' do
+  shared_examples 'renders a successful response' do
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /index' do
+    include_context 'when user signed in'
+
+    before do
+      get space_news_index_path
+    end
+
+    include_examples 'renders a successful response'
+  end
+
+  describe 'GET /search' do
+    include_context 'when user signed in'
+
+    context 'with valid parameters' do
       before do
-        get space_news_index_path
+        get search_space_news_index_path, params: { title: 'status', commit: 'Search' }
       end
 
       include_examples 'renders a successful response'
     end
 
-    describe 'GET /search' do
+    context 'with invalid parameters' do
       before do
-        get search_space_news_index_path, params: { title: 'status', commit: 'Search' }
+        get search_space_news_index_path, params: { title: Faker::Lorem.characters, commit: 'Search' }
       end
 
       include_examples 'renders a successful response'
